de9076f42fe176931c50990b8e5a4548f0dcc1df
sjamescarter <stephenjamescarter@gmail.com>
2023-11-02 15:35:41 -0500

    commit tree 7d37388dd0ca0c30a35db82efc0f6b7e16538609

--- src/features/books/booksSlice.js
+++ src/features/books/booksSlice.js
@@ -1,18 +1,4 @@
 // Action Creators
-export const addAuthor = (author) => {
-  return {
-    type: "authors/add",
-    payload: author,
-  };
-};
-
-export const removeAuthor = (id) => {
-  return {
-    type: "authors/remove",
-    payload: id,
-  };
-};
-
 export const addBook = (book) => {
   return {
     type: "books/add",
@@ -29,11 +15,10 @@ export const removeBook = (id) => {
 
 // Reducers
 const initialState = {
-  authors: [], //array of authors
   books: [], // array of books
 };
 
-export default function reducer(state = initialState, action) {
+export default function booksReducer(state = initialState, action) {
   switch (action.type) {
     case "books/add":
       return {
@@ -46,21 +31,6 @@ export default function reducer(state = initialState, action) {
       return {
         ...state,
         books: newBooks,
-      };
-
-    case "authors/add":
-      return {
-        ...state,
-        authors: [...state.authors, action.payload],
-      };
-
-    case "authors/remove":
-      const newAuthors = state.authors.filter(
-        (author) => author.id !== action.payload
-      );
-      return {
-        ...state,
-        authors: newAuthors,
       };
 
     default:
